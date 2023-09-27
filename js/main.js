cb76518c1f8d0d60fdde3183f71cbe9ce4cda19e
Pedro Augusto <pedroaugusto.cg19@gmail.com>
2023-09-27 15:46:05 -0300

    mobile-nav implementada com animações

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 window.onload = function () {
+    let menuAtivo = false;
 
     window.addEventListener("scroll", function (e) {
 
@@ -12,10 +13,21 @@ window.onload = function () {
 
     const menu_btn = document.querySelector(".hamburger");
     const mobile_menu = document.querySelector(".mobile-nav");
-
+    const black_box = document.querySelector(".black-box");
 
     menu_btn.addEventListener("click", function () {
         menu_btn.classList.toggle('is-active');
-        //mobile_menu.classList.toggle('is-active');
-    }
+        mobile_menu.classList.toggle('is-active');
+
+        menuAtivo = !menuAtivo;
+
+        if (menuAtivo) {
+            black_box.classList.add('is-active'); // Ativa o fade in
+            black_box.classList.remove('fadeout');
+        } else {
+            black_box.classList.remove('is-active'); // Remove o fade in
+            black_box.classList.add('fadeout');
+        }
+    });
+
 }
